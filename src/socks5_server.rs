631700dd822b8fe690e641a2cdd56a0943587ce7
cxk <1084282692@qq.com>
2023-06-17 20:02:33 +0800

    tcp && https ipv4 优先选择

--- src/socks5_server.rs
+++ src/socks5_server.rs
@@ -1,7 +1,7 @@
 use anyhow::{ensure, Context,bail,anyhow};
-use tokio::{io::{copy_bidirectional, self, AsyncReadExt,AsyncWriteExt}, net::{self,TcpStream, TcpSocket,lookup_host}};
+use tokio::{io::{copy_bidirectional, self, AsyncReadExt,AsyncWriteExt, AsyncRead, AsyncWrite}, net::{self,TcpStream, TcpSocket,lookup_host}};
 use log::{error,debug,warn};
-use std::net::SocketAddr;
+use std::{net::{SocketAddr}, fmt::Error};
 
 
 const VERSION: u8 = 0x05;
@@ -53,8 +53,22 @@ pub async fn handle_socks5_stream(mut s:TcpStream) {
             return;
         }
     }
-    
-    
+
+    let mut s2 = match get_cmd_sock(&mut s).await {
+        Ok(s2) => s2,
+        Err(e) => {
+            error!("verify socks5 err : {e}");
+            return ;
+        }
+    };
+
+    let num = copy_bidirectional(&mut s, &mut s2).await;
+    match num {
+        Ok((a,b)) => {
+            println!("end bidirectional {} - {}",a,b);
+        }
+        _ => return
+    }
 }
 
 async fn verify_socks5(s:&mut TcpStream) -> anyhow::Result<()> {
@@ -83,6 +97,126 @@ async fn verify_socks5(s:&mut TcpStream) -> anyhow::Result<()> {
     s.write(&[0x5,0x00]).await?;
     
     Ok(())
+}
+
+async fn get_cmd_sock(socket: &mut TcpStream) -> anyhow::Result<TcpStream> {
+    
+    let mut buf = [0;266];
+    socket.read_exact(&mut buf[..4]).await.context("context")?;
+
+    const CMD_CONNECT:u8 = 0x01;
+    const CMD_UDP:u8 = 0x03;
+
+    ensure!(buf[0] == VERSION,"invalid socks version!");
+    ensure!([CMD_CONNECT,CMD_UDP].contains(&buf[1]), "only support connect and udp: {}", buf[1]);
+    ensure!(buf[2] == 0x00, "connect: invalid RSV: {}", buf[2]);
+    
+    let addr = read_target_address(socket, buf[3]).await?;
+
+    let s2 = match buf[1] {
+        CMD_CONNECT => {
+            let socket2 = match addr {
+                SocketAddr::V4(_) => TcpSocket::new_v4().context("context")?,
+                SocketAddr::V6(_) => TcpSocket::new_v6().context("context")?,
+                _ => bail!("")
+            };
+            let tcp = socket2.connect(addr).await.context("context")?;
+            tcp
+        },
+        _ => bail!("invalid cmd {}",buf[1])
+    };
+    const ATYP_IP_V4: u8 = 0x01;
+    const ATYP_IP_V6: u8 = 0x04;
+    let mut buf = [0;256];
+    buf[0] = VERSION;
+    let reply = match addr {
+        SocketAddr::V4(a) => {
+            buf[3] = ATYP_IP_V4;
+            let ip = a.ip().octets();
+            let port = a.port().to_ne_bytes();
+            buf[4..8].copy_from_slice(&ip);
+            buf[8..10].copy_from_slice(&port);
+            &buf[..10]
+        },
+        SocketAddr::V6(a) => {
+            buf[3] = ATYP_IP_V4;
+            let ip = a.ip().octets();
+            let port = a.port().to_ne_bytes();
+            buf[4..20].copy_from_slice(&ip);
+            buf[20..22].copy_from_slice(&port);
+            &buf[..22]
+        }
+        _ => bail!("")
+    };
+    socket.write(reply).await?;
+    println!("get connected socket");
+    Ok(s2)
+}
+
+async fn read_target_address(socket: &mut TcpStream,atyp:u8) -> anyhow::Result<SocketAddr> {
+    
+    const ATYP_IP_V4: u8 = 0x01;
+    const ATYP_IP_V6: u8 = 0x04;
+    const ATYP_DOMAIN: u8 = 0x03;
+
+    // let mut buf = [0; 255];
+    let addr: SocketAddr = match atyp {
+        ATYP_IP_V4 => {
+            let mut ip = [0; 4];
+            socket
+                .read_exact(&mut ip)
+                .await
+                .context("connect: read ipv4 addr")?;
+
+            let port = socket.read_u16().await.context("connect: read ipv4 port")?;
+            let addr = (ip, port).into();
+
+            addr
+        },
+        ATYP_IP_V6 => {
+            let mut ip = [0; 16];
+            socket.read_exact(&mut ip).await.context("connect: read ipv6 addr")?;
+
+            let port = socket.read_u16().await.context("connect: read ipv6 port")?;
+            let addr = (ip, port).into();
+            
+            addr
+        }
+        ATYP_DOMAIN => {
+            let mut buf = [0;256];
+            let n = socket
+                .read_u8()
+                .await
+                .context("connect: read domainname length")? as usize;
+
+            let domain_name = &mut buf[..n];
+
+            socket.read_exact(domain_name).await.context("connect: read domainname")?;
+
+            let domain_name =
+                std::str::from_utf8(domain_name).context("connect: str::from_utf8(domain_name)")?;
+
+            let port = socket.read_u16().await.context("connect: read domain_name port")?;
+
+            debug!( "{} - connect to: {}:{}", tcp_stream_addrs(socket, false), domain_name, port );
+
+            let iter = lookup_host((domain_name, port)).await.context("connect: lookup_host")?;
+            
+            //https 仅支持 ipv4 所以选择 ipv4 优先
+            let mut addr = None;
+            for a in iter {
+                addr = Some(a);
+                if a.is_ipv4() {
+                    break;
+                }
+            };
+            
+            addr.ok_or_else(|| anyhow!("connect: lookup_host: empty: {}:{}", domain_name, port))?
+        }
+        _ => bail!("invalid atyp {}",atyp)
+    };
+    debug!( "{} - connect to: {}", tcp_stream_addrs(socket, false), addr );
+    Ok(addr)
 }
 
 async fn get_target_stream(socket:&mut TcpStream) -> anyhow::Result<TcpStream> {
